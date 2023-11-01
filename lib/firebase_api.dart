23bc2f58b405085def4785dc0e44e25c9bef9015
fatemeh <akhlaghi.fatemeh@gmail.com>
2023-11-01 17:18:12 +0330

    Add local notification

--- lib/firebase_api.dart
+++ lib/firebase_api.dart
@@ -1,10 +1,21 @@
+import 'dart:convert';
+
 import 'package:fcm_example/main.dart';
 import 'package:fcm_example/notification_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: "This channel is used for important notifications",
+    importance: Importance.defaultImportance,
+  );
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
 
   Future<void> initNotifications() async {
     // On iOS it shows a dialog
@@ -16,6 +27,7 @@ class FirebaseApi {
     print('Token: $fCMToken'); // In real app we could save it into DB
 
     initPushNotifications();
+    initLocalNotifications();
   }
 
   void handleMessage(RemoteMessage? message) {
@@ -38,6 +50,42 @@ class FirebaseApi {
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if(notification == null) return;
+
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap())
+      );
+    });
+  }
+
+  Future initLocalNotifications() async {
+    const iOS = IOSInitializationSettings(); // This should be changed in new versio
+    const android = AndroidInitializationSettings('@drawable/ic_launcher');
+    const settings = InitializationSettings(android: android, iOS: iOS);
+
+    await _localNotifications.initialize(
+      settings,
+      onSelectNotification: (payload) { // This should be changed in new version
+        final message = RemoteMessage.fromMap(jsonDecode(payload.toString()));
+        handleMessage(message);
+      }
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
   }
 }
 
